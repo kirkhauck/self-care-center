238b6342e4d35d7e2567cccbb4fbeb570028d74f
Kirk Hauck <kirkihauck@gmail.com>
2022-12-15 14:30:42 -0700

    Add script link to messages.js in index.html

---
+++ src/main.js
@@ -0,0 +1,9 @@
+var receiveMessageButton = document.querySelector('.receive-message-button')
+
+// if(typeof window !== 'undefined') {
+//   console.log('You are on the browser')
+// } else {
+//   console.log('You are on the server')
+// }
+
+console.log(receiveMessageButton);
